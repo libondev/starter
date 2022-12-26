7855bc497a61638c48a7bceda0f7f4384d6c3e95
Chestnut <47918504+banlify@users.noreply.github.com>
2022-12-26 21:52:37 +0800

    Update vite.config.ts

--- ts-vue-lite/vite.config.ts
+++ ts-vue-lite/vite.config.ts
@@ -12,7 +12,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true
+    }),
     vueJsx(),
     unocss({
       presets: [
