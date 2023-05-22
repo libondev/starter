a11556ae93b2e815a3363766cede93c4c844d62e
Libon <47918504+banlify@users.noreply.github.com>
2023-05-22 15:47:54 +0800

    chore: Create index.vue

---
+++ vue/src/components/Computer/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-full flex items-center justify-center text-center">
+    <div>
+      <p> {{ count }} </p>
+
+      <button class="px-4 py-1 bg-#fff rounded b-1 cursor-pointer active:bg-#ccc" @click="accretion">Plus</button>
+      <button class="px-4 py-1 bg-#fff rounded b-1 cursor-pointer active:bg-#ccc ml-2" @click="reduction">Minus</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { accretion, count, reduction } = useComputer()
+</script>
