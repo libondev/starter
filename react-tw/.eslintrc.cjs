ebe0964241ff1e21cd153a6c242d67e893e72103
libondev <bon.li@outlook.com>
2023-09-16 13:47:39 +0800

    chore: add react-tw components

--- react-tw/.eslintrc.cjs
+++ react-tw/.eslintrc.cjs
@@ -11,6 +11,6 @@ module.exports = {
   plugins: ['react-refresh'],
 
   rules: {
-    'curly': ['error', 'all'],
+    curly: ['error', 'all'],
   },
 }
--- react-tw/src/pages/index.tsx
+++ react-tw/src/pages/index.tsx
@@ -7,8 +7,13 @@ export default function Home() {
     <div className="">
       <h1>1231321</h1>
       <ModeToggle />
+      <div className='flex gap-4 m-4'>
       <Button>12312</Button>
-      1231312312
+      <Button variant='destructive'>12312</Button>
+      <Button variant='ghost'>12312</Button>
+      <Button variant='link'>12312</Button>
+      <Button variant='outline'>12312</Button>
+      <Button variant='secondary'>12312</Button></div>
 
       <img src="https://so1.360tres.com/dr/270_500_/t01e2198cad53ecdf1b.jpg?size=268x201" />
 
