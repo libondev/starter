d15ea4989019cf4e79ab4bb2274741642c509be0
libondev <bon.li@outlook.com>
2023-10-11 11:33:28 +0800

    chore: update eslint config

--- vue-tw/eslint.config.js
+++ vue-tw/eslint.config.js
@@ -4,7 +4,10 @@ export default antfu(
   {
     ignores: ['.DS_Store', '*.d.ts'],
     rules: {
+      'curly': ['error', 'all'], // 'multi' , 'consistent'
       'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }],
     },
   },
   //  {
