670b2034267a18d9c684743751ebead20e587920
libondev <bon.li@outlook.com>
2023-10-22 22:03:13 +0800

    chore: update pkg

--- vue-tw/src/components/ui/select/SelectTrigger.vue
+++ vue-tw/src/components/ui/select/SelectTrigger.vue
@@ -7,7 +7,7 @@ const props = defineProps<SelectTriggerProps & { invalid?: boolean }>()
 <template>
   <SelectTrigger
     v-bind="props"
-    class="flex h-8 w-full items-center justify-between rounded-md border border-input focus:border-primary bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+    class="flex h-8 w-full items-center justify-between truncate rounded-md border border-input focus:border-primary bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
     :class="props.invalid && '!ring-destructive ring-2 placeholder:!text-destructive'"
   >
     <slot />
