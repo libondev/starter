1475421c7253f1b0a9aeaffb94ffd1921b4870a5
libondev <bon.li@outlook.com>
2024-08-04 16:37:26 +0800

    chore: update dark mode key

--- vue/src/layouts/components/ToggleMode.vue
+++ vue/src/layouts/components/ToggleMode.vue
@@ -3,7 +3,7 @@ import { useDark, useToggle } from '@vueuse/core'
 import { name } from '@/../package.json'
 
 // 如果用户没有设置过主题选项(包含第一次进入系统)，那么就使用系统的明暗主题设置
-const isDark = useDark({ storageKey: `${name}.fe.color-mode` })
+const isDark = useDark({ storageKey: `${name}.system.color-mode` })
 const toggleDark = useToggle(isDark)
 </script>
 
