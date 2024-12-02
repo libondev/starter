9960f9eeda434274fa8c78460242d8cb05274751
Libon <bon.li@outlook.com>
2024-12-02 16:52:36 +0800

    chore: update dep

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -8,9 +8,6 @@ export default {
   // prefix: '',
 
   content: [
-    './pages/**/*.{ts,tsx,vue}',
-    './components/**/*.{ts,tsx,vue}',
-    './app/**/*.{ts,tsx,vue}',
     './src/**/*.{ts,tsx,vue}',
   ],
 
