7a92993efd0aef0f91435a4995a698ab41d3a486
Libon <bon.li@outlook.com>
2024-12-02 20:55:45 +0800

    chore: import order

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -3,8 +3,8 @@ import generatedRoutes from 'virtual:generated-pages'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import { useHeadTitle } from './guards/use-head-title.ts'
-// import { useUserAuth } from './guards/user-auth.ts'
 import { useProgressBar } from './guards/use-progress-bar.ts'
+// import { useUserAuth } from './guards/use-user-auth.ts'
 import { useViewTransition } from './guards/use-view-transition.ts'
 
 const router = createRouter({
