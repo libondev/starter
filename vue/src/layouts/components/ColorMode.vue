da48bf455be06cd9ebe411e19587edf7e26ad80a
libondev <bon.li@outlook.com>
2024-12-19 11:33:29 +0800

    feat: support system color mode

--- vue/src/layouts/components/ColorMode.vue
+++ vue/src/layouts/components/ColorMode.vue
@@ -1,30 +1,64 @@
 <script lang="ts" setup>
-import { useToggle } from '@vueuse/core'
+import { useMediaQuery } from '@vueuse/core'
+import { DeviceAlternateIcon, MoonIcon, SunIcon } from 'gdsi/vue'
 
-const isDark = customRef((track, trigger) => {
-  let _dark = document.documentElement.classList.contains('dark')
+const colorModes = {
+  auto: {
+    render: DeviceAlternateIcon,
+    transition: 'dark',
+  },
+  dark: {
+    render: MoonIcon,
+    transition: 'light',
+  },
+  light: {
+    render: SunIcon,
+    transition: 'auto',
+  },
+} as const
+
+type ColorMode = keyof typeof colorModes
+
+const isPrefersDark = useMediaQuery('(prefers-color-scheme: dark)')
+
+const colorMode = customRef<ColorMode>((track, trigger) => {
+  const root = document.documentElement
+  const storageKey = 'fe.system.color-mode'
+  let curMode = localStorage.getItem(storageKey) as ColorMode || 'auto'
 
   return {
     get() {
       track()
-      return _dark
+      return curMode
     },
-    set(value) {
-      _dark = value
-      document.documentElement.classList.toggle('dark', value)
-      localStorage.setItem('fe.system.color-mode', value ? 'dark' : 'auto')
+    set(newMode) {
+      if (newMode === curMode)
+        return
+
+      const newAppliedMode = getAppliedMode(newMode, isPrefersDark.value)
+
+      root.classList.remove(curMode)
+      root.classList.add(newAppliedMode)
+
+      localStorage.setItem(storageKey, newMode)
+      curMode = newMode
 
       trigger()
     },
   }
 })
 
-const toggleDark = useToggle(isDark)
+function getAppliedMode(mode: ColorMode, isDark: boolean) {
+  return mode === 'auto' ? (isDark ? 'dark' : 'light') : mode
+}
+
+function toggleColorMode() {
+  colorMode.value = colorModes[colorMode.value].transition
+}
 </script>
 
 <template>
-  <PButton variant="outline" @click="toggleDark()">
-    <IGdsSun v-if="isDark" />
-    <IGdsMoon v-else />
+  <PButton variant="outline" @click="toggleColorMode()">
+    <Component :is="colorModes[colorMode].render" />
   </PButton>
 </template>
