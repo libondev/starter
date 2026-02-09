8e48ac87e0cd5136c7067b3e154cc753f016967f
libondev <bon.li@outlook.com>
2026-02-09 15:42:21 +0800

    feat: support custom website name

--- vue/src/router/guards/use-head-title.ts
+++ vue/src/router/guards/use-head-title.ts
@@ -1,14 +1,14 @@
 import type { Router } from 'vue-router'
 
-import { name } from '@/../package.json'
-
 export function useHeadTitle(router: Router) {
+  const websiteName = import.meta.env.VITE_WEBSITE_NAME
+
   router.beforeEach((to, _, next) => {
     let title = (to.meta.title as string) || ''
 
     title &&= `${title} - `
 
-    document.title = title + name
+    document.title = title + websiteName
     next()
   })
 }
